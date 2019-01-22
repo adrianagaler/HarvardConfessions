11664ef7eb2939f9ad1e8c4a3f63a310cd4960ba
joviharvard <tanj@college.harvard.edu>
2019-01-22 16:29:33 -0500

    edited models

--- src/models/Comment.js
+++ src/models/Comment.js
@@ -8,14 +8,23 @@ class Comment extends BaseModel {
 
     static get relationMappings () {
         const Post = require('./Post');
+        const User = require('./User');
 
         return {
-            posts: {
+            post: {
                 relation: BelongsToOneRelation,
                 modelClass: Post,
                 join: {
                     from : 'comments.postId'
                     to: 'posts.id'
+                },
+            },
+            user: {
+                relation: BelongsToOneRelation,
+                modelClass: User,
+                join: {
+                    from: 'comments.author',
+                    to: 'users.id'
                 },
             },
         }

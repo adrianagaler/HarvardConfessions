08b4f5e2bb3148e67c5ae1bd4e2fe12f83c034e0
Adriana172 <adrianarotaru@college.harvard.edu>
2019-08-08 13:04:01 +0200

    Updates

---
+++ precache-manifest.33b8092b8ae663fdb91b268d5040d759.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "119042fb4b44cb67ae4d92011c3011fb",
+    "url": "/HarvardConfessions/index.html"
+  },
+  {
+    "revision": "636aba22ecb492ecb285",
+    "url": "/HarvardConfessions/static/js/2.e9673f60.chunk.js"
+  },
+  {
+    "revision": "2a9123bea2c33bb3c1bb",
+    "url": "/HarvardConfessions/static/js/main.2afa3322.chunk.js"
+  },
+  {
+    "revision": "d44107e254913afa2df4",
+    "url": "/HarvardConfessions/static/js/runtime~main.b5b9061f.js"
+  }
+]);
